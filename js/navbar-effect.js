15bb4f8121050bd2bc313749327f15fd52ac4048
Phil <philip.zuschlag@gmail.com>
2019-01-25 16:32:27 +0100

    Add navbar and navbar-effect.

---
+++ js/navbar-effect.js
@@ -0,0 +1,17 @@
+var navBar = document.getElementsByTagName("nav")[0];
+
+function navBarEffect() {
+  if (document.body.scrollTop >= navBar.offsetHeight || document.documentElement.scrollTop >= navBar.offsetHeight) {
+    navBar.classList.add("scrolled");
+  } else {
+    navBar.classList.remove("scrolled");
+  }
+}
+
+
+window.addEventListener('scroll', navBarEffect);
+
+// window.onscroll = function () {
+//   moveAnimals();
+//   navBarEffect();
+// };
