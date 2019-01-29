ad32b79cbdf46d75fc351384391437c15cfcb96b
Phil <philip.zuschlag@gmail.com>
2019-01-29 13:49:05 +0100

    add js for move animals | just only move the fire.. not the animal.. and it doesn't work.. so WIP

---
+++ js/moveAnimals.js
@@ -0,0 +1,18 @@
+var animals = document.getElementsByClassName("move");
+var windowHeight = document.documentElement.clientHeight;
+
+function moveAnimals(animal) {
+  var windowTopPos = document.documentElement.scrollTop;
+
+  if (((windowTopPos + windowHeight) > animal.offsetTop) && ((windowTopPos) < (animal.offsetTop + animal.offsetHeight))) {
+  
+    var dist = (windowTopPos + windowHeight / 2) - (animal.offsetTop + animal.offsetHeight / 2);
+    var ref = dist / (windowHeight / 10);
+  
+    animal.style.transform = 'translateY(' + (parseInt((ref * 10)/4)*-1) + "%)";
+  }
+}
+
+window.addEventListener('scroll', moveAnimals(animals[0]));
+window.addEventListener('scroll', moveAnimals(animals[1]));
+window.addEventListener('scroll', moveAnimals(animals[2]));
